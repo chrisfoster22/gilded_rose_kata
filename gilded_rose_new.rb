83c47f98193e718ed189856d46eada704443baa1
Chris Foster <fosterchris22@gmail.com>
2015-05-13 12:46:38 -0400

    All tests passing.

--- gilded_rose_new.rb
+++ gilded_rose_new.rb
@@ -1,46 +1,52 @@
 def update_quality(items)
-
-    ages_well = []
-    ages_poorly = []
-    conjured = []
     sulfuras = items.find { |i| i.name.include?("Sulfuras") }
     items.delete(sulfuras)
-    items.each do |i|
-        ages_well << i if i.name == 'Aged Brie' || i.name == 'Backstage passes to a TAFKAL80ETC concert'
-        conjured << i if i.name.include?('Conjured')
-        ages_poorly << i unless ages_well.include?(i) || conjured.include?(i) 
-    end
-
-    ages_well.each do |w|
-        unless w.quality >= 50
-            w.quality += 1
-            if w.name.include?("concert")
-                w.quality += 1 if w.sell_in < 11
-                w.quality += 1 if w.sell_in < 6
-                w.quality = 0 if w.sell_in <= 0
-            else
-                w.quality += 1 unless w.sell_in > 0 || w.quality >= 50 
-            end
-        end
-    end
-
-    conjured.each do |c|
-        unless c.quality == 0
-            c.quality -= 2
-            c.quality -= 2 if c.sell_in < 1
-        end
-    end
-
-    ages_poorly.each do |p|
-        unless p.quality == 0
-            p.quality -= 1
-            p.quality -= 1 if p.sell_in < 1
-        end
-    end
-
-    items.each do |i|   
-        i.sell_in -= 1
-    end
+  QualityUpdater.new.update(items)
 end
+
+class QualityUpdater
+  def update(items)
+    items.each do |i|
+      i.quality -= 1 
+      i.quality -= 1 if i.sell_in <= 0
+      AgedUpdater.new.update(i) if i.name.include?('Aged')
+     ConcertUpdater.new.update(i) if i.name.include?('concert')
+      ConjuredUpdater.new.update(i) if i.name.include?('Conjured')
+      i.sell_in -= 1
+      reset_item(i)
+    end 
+  end
+
+  def reset_item(item)
+    item.quality = 0 if item.quality < 0
+    item.quality = 50 if item.quality > 50
+  end
+end
+
+class AgedUpdater
+  def update(item)
+    item.quality += 2
+    item.quality += 2 if item.sell_in <= 0
+  end
+end
+
+class ConcertUpdater
+  def update(item)
+    item.quality += 2 
+    item.quality += 1 if item.sell_in < 11
+    item.quality += 1 if item.sell_in < 6
+    item.quality = 0 if item.sell_in < 1
+  end
+end
+
+class ConjuredUpdater
+  def update(item)
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 1
+  end
+end
+    
+
+
 
 Item = Struct.new(:name, :sell_in, :quality)
